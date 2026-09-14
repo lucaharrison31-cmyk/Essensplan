f9b2b03c2c2ef6a797ac579b286834f007406996
lucaharrison31-cmyk <lucaharrison31@gmail.com>
2026-09-14 14:29:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='essensplan-v1';
+const FILES=['./','./essensplan.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
